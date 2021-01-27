fd969c168db2476b0169a392d6b79b6b58cdb003
(hyebin) <(sksp4334@naver.com)>
2021-01-28 00:04:59 +0900

    add express and page distribution

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/property", (req, res) => {
+  res.sendFile(__dirname + "/property.html");
+});
+app.get("/pages", (req, res) => {
+  res.sendFile(__dirname + "/pages.html");
+});
+
+app.listen(port, () => {
+  console.log(`server is listening at localhost:3000`);
+});
